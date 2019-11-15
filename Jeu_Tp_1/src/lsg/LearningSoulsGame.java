4440ab7adac4738bdbb2544e4fb3508e76876424
Corleone <corleonedany@live.fr>
2019-11-15 10:56:51 +0100

    Rectifications suite a la modification du package

---
+++ Jeu_Tp_1/src/lsg/LearningSoulsGame.java
@@ -0,0 +1,32 @@
+package lsg;
+
+public class LearningSoulsGame {
+	public static void main(String[] args) {
+		Hero Nero = new Hero("Nero");
+		Hero PontMage = new Hero("PontMage");
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		new Monster();
+		
+		Nero.PrintStats();
+		PontMage.PrintStats();
+		
+	}
+}
