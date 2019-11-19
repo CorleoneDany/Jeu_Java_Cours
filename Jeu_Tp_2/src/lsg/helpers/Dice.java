a21af9aa58a0b8cad2f10478cd5bba68140f5e14
Corleone <corleonedany@live.fr>
2019-11-19 15:51:00 +0100

    Ajout de la méthode main, tentatives de résolution de l'exercice 2.4

--- Jeu_Tp_2/src/lsg/helpers/Dice.java
+++ Jeu_Tp_2/src/lsg/helpers/Dice.java
@@ -17,15 +17,27 @@ public class Dice {
 	
 	public Integer roll() {
 		Integer random_num = random.nextInt(faces);
-		//System.out.print(random_num);
+		//System.out.println(random_num);
 		return random_num;	
 	}
 	
 	public String main() {
 		String test = "";
+		Integer val_min = 0;
+		Integer val_max = 0;
+		String[] val_tab;
 		for(Integer i = 0; i < 500; i++) {
 			i++;
 			test = test + roll() + " ";
+		}
+		val_tab = test.split(" ");
+		for(Integer i = 0; i < val_tab.length; i++) {
+			if((Integer.parseInt(val_tab[i]) < val_min)) {
+				val_min = Integer.parseInt(val_tab[i]);
+			}
+			else if((Integer.parseInt(val_tab[i]) > val_max)) {
+				val_max = Integer.parseInt(val_tab[i]);
+			}
 		}
 		System.out.print(test);
 		return test;
